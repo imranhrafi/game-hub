6dceb29d34a3cb818089f74211bac9fabaf24f9d
imranhrafi <senbagiimran@gmail.com>
2023-05-22 23:58:37 +0600

    Show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,33 +20,38 @@ interface FetchGamesResponse {
   results: Game[];
 }
 
-const useGames = (): { games: Game[]; error: string } => {
-  // Set up state variables to store the fetched games and any errors.
+const useGames = (): {
+  games: Game[];
+  error: string;
+  isLoading: boolean;
+} => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    // Create a new AbortController instance to handle cancellation of the API request.
     const controller = new AbortController();
 
-    // Send a GET request to the API to fetch the games data, passing in the AbortController's signal.
+    setLoading(true);
+
     apiClient
       .get<FetchGamesResponse>("/games", {
         signal: controller.signal,
       })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
-        // Handle any errors thrown by the API request, ignoring any cancellations due to aborting.
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
-    // Return a cleanup function that will abort the API request if the component unmounts or the dependencies change.
     return () => controller.abort();
   }, []);
 
-  // Return the fetched games and any error messages as an object.
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
